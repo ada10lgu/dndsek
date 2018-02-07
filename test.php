540873a2aa0c79db13998453fc2d59a3915b4c7d
Lars Gustafson <lars.gustafson@gmail.com>
2018-02-07 10:28:59 +0100

    minor uppdate

--- test.php
+++ test.php
@@ -4,7 +4,8 @@
 
 </head>
 <body>
-fsd<canvas id="myCanvas" width="800" height="300" style="border:1px solid #000000;">
+<br/>
+fsdfsdfsddfds<canvas id="myCanvas" width="800" height="300" style="border:1px solid #000000;">
 </canvas>
 
 <script>
@@ -14,9 +15,15 @@ var m = false;
 var centerX = c.width/2;
 var centerY = c.height/2; 
 
-
 var prevX = 0;
 var prevY = 0;
+
+var size = 20;
+var width = 15;
+var height = 5;
+
+var mouseX = 0;
+var mouseY = 0;
 
 c.addEventListener("mousemove", moving,false);
 c.addEventListener("mouseup", released,false);
@@ -25,14 +32,17 @@ c.addEventListener("mousedown",pressed ,false);
 
 draw(0,0);
 function moving(e) {
-    if (!m)
-        return;
-    centerX += e.x - prevX;
-    centerY += e.y - prevY;
+    var rect = c.getBoundingClientRect();
+    mouseX = Math.round(e.x - rect.left);
+    mouseY = Math.round(e.y - rect.top);
 
-    prevX = e.x;
-    prevY = e.y;
+    if (m) {
+        centerX += e.x - prevX;
+        centerY += e.y - prevY;
 
+        prevX = e.x;
+        prevY = e.y;
+    }
     draw();
 }
 
@@ -44,8 +54,9 @@ function pressed(e) {
 
 function released(e) {
     m = false;
-    var x = e.x + c.clientLeft;
-    var y = e.y + c.clientTop;
+    var rect = c.getBoundingClientRect();
+    var x = e.x - rect.left;
+    var y = e.y - rect.top;
     
     if (x >=10 && x <= 95 && y >= 10 && y <= 30) {
         centerX = c.width/2;
@@ -58,14 +69,14 @@ function released(e) {
 function draw() {
     var ctx = c.getContext("2d");
     ctx.clearRect(0, 0, c.width, c.height);
-    var size = 20;
+    
     
     var w = size*2;
     var h = Math.sqrt(3)/2 * w;
 
     ctx.beginPath();
-    for (x = -3; x < 3; x++) { 
-        for (y = -3; y < 3; y++) { 
+    for (x = -width; x <= width; x++) { 
+        for (y = -height; y <= height; y++) { 
             a = centerX + x*3/4*w;
             b = centerY + y*h + Math.abs(x)%2 * 1/2*h;
 
@@ -82,13 +93,32 @@ function draw() {
     ctx.stroke();
 
     
-    
+       
+    {
+        xx = 10;
+        yy = 10;
+        ww = 85;
+        hh = 20;
+        ctx.clearRect(xx,yy,ww,hh);
+        ctx.strokeRect(xx,yy,ww,hh);
+        ctx.font = "12px Arial";
+        ctx.fillText("Center to base",xx+2,yy+hh-5);
+    }
+    {
+        xx = 10;
+        yy = c.height - 30;
+        ww = 85;
+        hh = 20;
+        ctx.clearRect(xx,yy,ww,hh);
+        ctx.strokeRect(xx,yy,ww,hh);
+        ctx.font = "12px Arial";
+        ctx.fillText("x: " + (mouseX-centerX) + " y: " + (mouseY-centerY),xx+2,yy+hh-5);
+    }
     
 
-    ctx.clearRect(10, 10, 85, 20);
-    ctx.strokeRect(10,10,85,20 );
-    ctx.font = "12px Arial";
-    ctx.fillText("Center to base",12,25);
+
+   
+
 }
 
 
